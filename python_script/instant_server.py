20cafa6ca5764e14e370a894745a269df40077aa
hidetsugu-takahashi <hidetsugu.takahashi@hakuhodo.co.jp>
2017-07-12 18:07:11 +0900

    :sparkles: サーバー立ち上げてQRコードまで出してくれるスクリプト作った。

---
+++ python_script/instant_server.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python3
+
+"""
+###############################
+このスクリプトを起動する前に。
+###############################
+
+以下コマンドをやらないと行けないです。
+
+```
+brew install python3
+pip3 install Pillow
+pip3 install qrcode
+```
+"""
+
+import socket
+import socketserver
+import http.server
+import qrcode
+
+myip = socket.gethostbyname(socket.gethostname())
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+code_img = qrcode.make('http://' + myip + ':' + str(PORT))
+code_img.show()
+
+with socketserver.TCPServer((myip, PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
